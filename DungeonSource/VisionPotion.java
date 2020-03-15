03c8759014d1b85f789a5b7dfe0c9c8c5fc97a89
wxyzsupermod <wxyzsupermod@gmail.com>
2020-03-15 14:18:22 -0700

    vision Potion attempting to resolve the issues with /n

--- DungeonSource/VisionPotion.java
+++ DungeonSource/VisionPotion.java
@@ -1,92 +1,13 @@
 import java.lang.Math;
 public class VisionPotion {
+	// Problem is that you are printing the things correctly but not actually printing the
+	// array correctly so try to print the array and check if the values are in the correct range to be shown
+	// then and only then can you print the room
 	public VisionPotion(){}
 	public void printVisionPotion(Room[][] rooms, int posX, int posY) {
 		for(int x = 0; x < rooms.length; x++) {
 			for(int y = 0; y < rooms.length; y++) {
-				if(Math.abs(posX - x) > 1 || Math.abs(posY - y) > 1) {
-					if(posX - 1 < 0) {
-						if(posY - 1 < 0) {
-							//top left corner
-							System.out.println(rooms[posX][posY].toString());
-							System.out.println(rooms[posX + 1][posY].toString());
-							System.out.println(rooms[posX + 1][posY + 1].toString());
-							System.out.println(rooms[posX][posY + 1].toString());
-						}
-						else if(posY + 1 > rooms[5].length) {
-							//bottom left corner
-							System.out.println(rooms[posX][posY].toString());
-							System.out.println(rooms[posX + 1][posY].toString());
-							System.out.println(rooms[posX + 1][posY - 1].toString());
-							System.out.println(rooms[posX][posY - 1].toString());
-						}
-						//against left wall
-						else {
-						System.out.println(rooms[posX][posY].toString());
-						System.out.println(rooms[posX + 1][posY].toString());
-						System.out.println(rooms[posX + 1][posY + 1].toString());
-						System.out.println(rooms[posX][posY + 1].toString());
-						System.out.println(rooms[posX + 1][posY - 1].toString());
-						System.out.println(rooms[posX][posY - 1].toString());
-						}
-					}
-					else if(posX + 1 > rooms.length) {
-						if(posY - 1 < 0) {
-							//top right corner
-							System.out.println(rooms[posX][posY].toString());
-							System.out.println(rooms[posX - 1][posY].toString());
-							System.out.println(rooms[posX][posY + 1].toString());
-							System.out.println(rooms[posX - 1][posY + 1].toString());
-						}
-						else if(posY + 1 > rooms[5].length) {
-							//bottom right corner
-							System.out.println(rooms[posX][posY].toString());
-							System.out.println(rooms[posX - 1][posY].toString());
-							System.out.println(rooms[posX - 1][posY - 1].toString());
-							System.out.println(rooms[posX][posY - 1].toString());
-						}
-						//against right wall
-						else {
-						System.out.println(rooms[posX][posY].toString());
-						System.out.println(rooms[posX][posY - 1].toString());
-						System.out.println(rooms[posX][posY + 1].toString());
-						System.out.println(rooms[posX - 1][posY].toString());
-						System.out.println(rooms[posX - 1][posY - 1].toString());
-						System.out.println(rooms[posX - 1][posY + 1].toString());
-						}
-					}
-					else if(posY + 1 > rooms[5].length) {
-						//agaisnt bottom wall
-						System.out.println(rooms[posX][posY].toString());
-						System.out.println(rooms[posX - 1][posY].toString());
-						System.out.println(rooms[posX - 1][posY - 1].toString());
-						System.out.println(rooms[posX][posY - 1].toString());
-						System.out.println(rooms[posX + 1][posY - 1].toString());
-						System.out.println(rooms[posX + 1][posY].toString());
-					}
-					else if(posY - 1 < 0) {
-						//against top wall
-						System.out.println(rooms[posX][posY].toString());
-						System.out.println(rooms[posX - 1][posY].toString());
-						System.out.println(rooms[posX + 1][posY].toString());
-						System.out.println(rooms[posX - 1][posY + 1].toString());
-						System.out.println(rooms[posX + 1][posY + 1].toString());
-						System.out.println(rooms[posX][posY + 1].toString());
-					}
-					else {
-						System.out.println(rooms[posX][posY].toString());
-						System.out.println(rooms[posX][posY + 1].toString());
-						System.out.println(rooms[posX][posY - 1].toString());
-						System.out.println(rooms[posX - 1][posY].toString());
-						System.out.println(rooms[posX - 1][posY + 1].toString());
-						System.out.println(rooms[posX - 1][posY - 1].toString());
-						System.out.println(rooms[posX + 1][posY].toString());
-						System.out.println(rooms[posX + 1][posY + 1].toString());
-						System.out.println(rooms[posX + 1][posY - 1].toString());
-					}
-				}
-				else
-					System.out.println("*");
+				System.out.print(rooms[x][y]);
 			}
 			System.out.println();
 		}
