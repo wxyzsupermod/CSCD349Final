743972953f64733faf087e73291184669b3726c4
ybaghlaf <61812592+ybaghlaf@users.noreply.github.com>
2020-03-15 14:26:19 -0700

    Changed for loop print in tester

--- DungeonSource/MYTESTERDELETELATER.java
+++ DungeonSource/MYTESTERDELETELATER.java
@@ -6,15 +6,19 @@ public class MYTESTERDELETELATER {
    
       Room[][] rooms = new Room[5][5];
       
+      System.out.print("**********************");
       for(int i =0; i < rooms.length; i++){
          System.out.println();  
          System.out.print("*");   
-         for(int j = 0; j < rooms.length; j++){
+         
+         for(int j = 0; j < rooms.length; j++){         
             rooms[i][j] = new Room();
-            
             System.out.print(rooms[i][j] + " ");
+            System.out.print("|");
          }
          System.out.print("*");
       }
+      System.out.println();
+      System.out.println("**********************");
    }
 }
