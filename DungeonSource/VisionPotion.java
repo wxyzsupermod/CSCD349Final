a8409e4cae33227b09e38518c270f389437b0043
sOvr9000 <jordanthegeek@gmail.com>
2020-03-17 14:33:19 -0700

    Fix VisionPotion

--- DungeonSource/VisionPotion.java
+++ DungeonSource/VisionPotion.java
@@ -1,8 +1,13 @@
-
-public class VisionPotion extends Potion {
+public class VisionPotion extends Potion{
+	public void printVisionPotion(int centerX, int centerY) {
+		DungeonAdventure.printRooms(centerX-1, centerY-1, centerX+1, centerY+1);
+	}
 	
 	@Override
 	public void affectCharacter(DungeonCharacter c) {
-		
+		Hero hero = (Hero) c;
+		if (hero != null) {
+			printVisionPotion(hero.getPosX(), hero.getPosY());
+		}
 	}
 }
