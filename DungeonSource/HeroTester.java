cd3f7fe31f177a0addd665be503b2ff162613e19
wxyzsupermod <wxyzsupermod@gmail.com>
2020-03-18 20:27:22 -0700

    Completed the Tester

--- DungeonSource/HeroTester.java
+++ DungeonSource/HeroTester.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.Random;
 
 import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
@@ -13,6 +14,161 @@ class HeroTester {
 	private Hero h1;
 	private Monster m1;
 	HeroFactory HF = new HeroFactory();
+	MonsterFactory MF = new MonsterFactory();
+	
+	@Test
+	public void testAddHitPointsDaedric() {
+		m1 = new Daedric();
+		m1.addHitPoints(5);
+		
+		assertEquals(75, m1.getHitPoints());
+	}
+	
+	
+	@Test
+	public void testAddHitPointsGremlin() {
+		m1 = new Gremlin();
+		m1.addHitPoints(5);
+		
+		assertEquals(75, m1.getHitPoints());
+	}
+	
+	
+	@Test
+	public void testAddHitPointsOgre() {
+		m1 = new Ogre();
+		m1.addHitPoints(5);
+		
+		assertEquals(205, m1.getHitPoints());
+	}
+	
+	@Test
+	public void testAddHitPointsSkeleton() {
+		m1 = new Skeleton();
+		m1.addHitPoints(5);
+		
+		assertEquals(105, m1.getHitPoints());
+	}
+	
+	@Test
+	public void testAddHitPointsZombie() {
+		m1 = new Zombie();
+		m1.addHitPoints(5);
+		
+		assertEquals(75, m1.getHitPoints());
+	}
+	
+	
+	
+	
+	// ++++++++++++++++++++++
+	
+	@Test 
+	public void testAddHitPointsElfWizard() {
+		h1 = new ElfWizard();
+		h1.addHitPoints(7);
+		
+		assertEquals(82, h1.getHitPoints());
+	}
+	
+	@Test
+	public void testSubtractHitPointsElfWizard() {
+		h1 = new ElfWizard();
+		h1.subtractHitPoints(4);
+		
+		assertEquals(71, h1.getHitPoints());
+	}
+	
+	
+	@Test 
+	public void testAddHitPointsGandalf() {
+		h1 = new Gandalf();
+		h1.addHitPoints(10);
+		
+		assertEquals(210, h1.getHitPoints());
+	}
+	
+	@Test
+	public void testSubtractHitPointsGandalf() {
+		h1 = new Gandalf();
+		h1.subtractHitPoints(9);
+		
+		assertEquals(191, h1.getHitPoints());
+	}
+	
+	@Test 
+	public void testAddHitPointsSorcerer() {
+		h1 = new Sorcerer();
+		h1.addHitPoints(1);
+		
+		assertEquals(201, h1.getHitPoints());
+	}
+	
+	@Test
+	public void testSubtractHitPointsSorcerer() {
+		h1 = new Sorcerer();
+		h1.subtractHitPoints(2);
+		
+		assertEquals(198, h1.getHitPoints());
+	}
+	
+	@Test 
+	public void testAddHitPointsSorceress() {
+		h1 = new Sorceress();
+		h1.addHitPoints(3);
+		
+		assertEquals(78, h1.getHitPoints());
+	}
+	
+	@Test
+	public void testSubtractHitPointsSorceress() {
+		h1 = new Sorceress();
+		h1.subtractHitPoints(2);
+		
+		assertEquals(73, h1.getHitPoints());
+	}
+	
+	@Test 
+	public void testAddHitPointsThief() {
+		h1 = new Thief();
+		h1.addHitPoints(6);
+		
+		assertEquals(81, h1.getHitPoints());
+	}
+	
+	@Test
+	public void testSubtractHitPointsThief() {
+		h1 = new Thief();
+		h1.subtractHitPoints(1);
+		
+		assertEquals(74, h1.getHitPoints());
+	}
+	
+	@Test 
+	public void testAddHitPointsWarrior() {
+		h1 = new Warrior();
+		h1.addHitPoints(1);
+		
+		assertEquals(126, h1.getHitPoints());
+	}
+	
+	@Test
+	public void testSubtractHitPointsWarrior() {
+		h1 = new Warrior();
+		h1.subtractHitPoints(5);
+		
+		assertEquals(120, h1.getHitPoints());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@Test
 	public void testHeroWarrior() {
 		h1 = new Warrior();
